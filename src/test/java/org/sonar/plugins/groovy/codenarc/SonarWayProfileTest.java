64f680eaf4540c13c816b5a0b8a6f95259a2d529
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-02-27 10:06:52 +0100

    Update test of sonar way profile

--- src/test/java/org/sonar/plugins/groovy/codenarc/SonarWayProfileTest.java
+++ src/test/java/org/sonar/plugins/groovy/codenarc/SonarWayProfileTest.java
@@ -49,7 +49,7 @@ public class SonarWayProfileTest {
     RulesProfile profile = profileDefinition.createProfile(messages);
 
     assertThat(profile.getName()).isEqualTo("Sonar way");
-    assertThat(profile.getActiveRules()).hasSize(61);
+    assertThat(profile.getActiveRules()).hasSize(59);
     assertThat(messages.hasErrors()).isFalse();
 
     CodeNarcRuleRepository repo = new CodeNarcRuleRepository(new XMLRuleParser());
