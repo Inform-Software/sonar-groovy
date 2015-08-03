ddda0eee96c5e2377803684957aa4bbe2a0082a0
Michael Gumowski <michael.gumowski@sonarsource.com>
2015-08-03 14:58:00 +0200

    SONARGROOV-2 Increase code coverage

---
+++ src/test/java/org/sonar/plugins/groovy/surefire/api/SurefireUtilsTest.java
@@ -0,0 +1,82 @@
+/*
+ * Sonar Groovy Plugin
+ * Copyright (C) 2010 SonarSource
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.groovy.surefire.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.batch.fs.FileSystem;
+import org.sonar.api.batch.fs.internal.DefaultFileSystem;
+import org.sonar.api.config.Settings;
+import org.sonar.api.resources.Project;
+import org.sonar.api.scan.filesystem.PathResolver;
+import org.sonar.api.test.MavenTestUtils;
+
+import java.io.File;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SurefireUtilsTest {
+
+  private Project project;
+  private FileSystem fs;
+  private PathResolver pathResolver;
+  private Settings settings;
+
+  @Before
+  public void setup() {
+    project = MavenTestUtils.loadProjectFromPom(getClass(), "shouldGetReportsFromProperty/pom.xml");
+    fs = new DefaultFileSystem(project.getFileSystem().getBasedir());
+    settings = mock(Settings.class);
+    pathResolver = new PathResolver();
+  }
+
+  @Test
+  public void should_get_reports_from_property() {
+    Settings settings = mock(Settings.class);
+    when(settings.getString("sonar.junit.reportsPath")).thenReturn("target/surefire");
+    assertThat(SurefireUtils.getReportsDirectory(settings, fs, pathResolver).exists()).isTrue();
+    assertThat(SurefireUtils.getReportsDirectory(settings, fs, pathResolver).isDirectory()).isTrue();
+  }
+
+  @Test
+  public void return_default_value_if_property_unset() throws Exception {
+    File directory = SurefireUtils.getReportsDirectory(settings, fs, pathResolver);
+    assertThat(directory.getCanonicalPath()).endsWith("target" + File.separator + "surefire-reports");
+    assertThat(directory.exists()).isFalse();
+    assertThat(directory.isDirectory()).isFalse();
+  }
+
+  @Test
+  public void return_default_value_if_can_not_read_file() throws Exception {
+    Settings settings = mock(Settings.class);
+    when(settings.getString("sonar.junit.reportsPath")).thenReturn("target/surefire");
+    PathResolver pathResolver = mock(PathResolver.class);
+    when(pathResolver.relativeFile(any(File.class), anyString())).thenThrow(new IllegalStateException());
+    File directory = SurefireUtils.getReportsDirectory(settings, fs, pathResolver);
+    assertThat(directory.getCanonicalPath()).endsWith("target" + File.separator + "surefire-reports");
+    assertThat(directory.exists()).isFalse();
+    assertThat(directory.isDirectory()).isFalse();
+  }
+
+}
---
+++ src/test/java/org/sonar/plugins/groovy/surefire/data/UnitTestResultTest.java
@@ -0,0 +1,51 @@
+/*
+ * Sonar Groovy Plugin
+ * Copyright (C) 2010 SonarSource
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.groovy.surefire.data;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UnitTestResultTest {
+  @Test
+  public void shouldBeError() {
+    UnitTestResult result = new UnitTestResult().setStatus(UnitTestResult.STATUS_ERROR);
+    assertThat(result.getStatus(), is(UnitTestResult.STATUS_ERROR));
+    assertThat(result.isError(), is(true));
+    assertThat(result.isErrorOrFailure(), is(true));
+  }
+
+  @Test
+  public void shouldBeFailure() {
+    UnitTestResult result = new UnitTestResult().setStatus(UnitTestResult.STATUS_FAILURE);
+    assertThat(result.getStatus(), is(UnitTestResult.STATUS_FAILURE));
+    assertThat(result.isError(), is(false));
+    assertThat(result.isErrorOrFailure(), is(true));
+  }
+
+  @Test
+  public void shouldBeSuccess() {
+    UnitTestResult result = new UnitTestResult().setStatus(UnitTestResult.STATUS_OK);
+    assertThat(result.getStatus(), is(UnitTestResult.STATUS_OK));
+    assertThat(result.isError(), is(false));
+    assertThat(result.isErrorOrFailure(), is(false));
+  }
+}
