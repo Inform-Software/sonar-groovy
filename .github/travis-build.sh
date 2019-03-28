76b55b865ea5e5420c6df6855e5ab44e77f3072a
Tobias Gruetzmacher <tobias.gruetzmacher@inform-software.com>
2019-03-28 07:27:44 +0100

    Only run SonarQube analysis on one Travis-CI matrix configuration

--- .github/travis-build.sh
+++ .github/travis-build.sh
@@ -5,14 +5,14 @@ add=
 if [ "$SONAR_VERSION" ]
 then
   add="$add -Dsonar.version=$SONAR_VERSION"
-fi
 
 # Workaround until https://jira.sonarsource.com/browse/TRAVIS-19 is fixed
 # See also https://community.sonarsource.com/t/travis-plugin-is-failing-on-external-pull-request/807/7
-if [ -n "$SONAR_SCANNER_HOME" ]
+elif [ -n "$SONAR_SCANNER_HOME" ]
 then
+  # Only run SonarQube analysis on one Travis-CI matrix configuration
   add="sonar:sonar $add"
 fi
 
-mvn -B -V -e verify $add
+mvn -B -V -e -Dstyle.color=always verify $add
 
