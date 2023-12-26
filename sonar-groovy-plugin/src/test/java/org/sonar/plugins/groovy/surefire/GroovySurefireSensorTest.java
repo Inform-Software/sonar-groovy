81d27e73415ac9ae7f8f9fa11956c1e82a3a779e
Amitoj Duggal <a.duggal@mytaxi.com>
2023-12-26 21:03:40 +0100

    Upgrade mockito, slf4j

--- sonar-groovy-plugin/src/test/java/org/sonar/plugins/groovy/surefire/GroovySurefireSensorTest.java
+++ sonar-groovy-plugin/src/test/java/org/sonar/plugins/groovy/surefire/GroovySurefireSensorTest.java
@@ -19,7 +19,7 @@
 package org.sonar.plugins.groovy.surefire;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
